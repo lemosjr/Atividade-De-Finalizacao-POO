7e3bedbdec52eb287a8d880734fe795067f56b42
lemosjr <160804009+lemosjr@users.noreply.github.com>
2025-08-11 19:29:53 -0300

    classe pai Funcionario

--- funcionario.py
+++ funcionario.py
@@ -1,3 +1,20 @@
 class Funcionario:
-    def __init__(self, ):
+    def __init__(self, nome, idade, cpf, cargo, contrato, setor):
+        self.nome = nome
+        self.idade = idade
+        self.cpf = cpf
+        self.cargo = cargo
+        self.contrato = contrato
+        self.setor = setor
+    
+    def __str__(self):
+        return f'''
+    nome: {self.nome}
+    idade: {self.idade}
+    cpf: {self.cpf}
+    cargo: {self.cargo}
+    contrato: {self.contrato}
+    setor: {self.setor}
+    
+    '''
         
